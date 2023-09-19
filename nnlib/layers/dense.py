20dab73257916de7ff613669e39b0dd8f19a877f
vitor-pires <79984208+vitorffpires@users.noreply.github.com>
2023-09-19 18:43:19 -0300

    test implementation
    Started to build the test of the library.

--- nnlib/layers/dense.py
+++ nnlib/layers/dense.py
@@ -1,7 +1,7 @@
 import numpy as np
-from Layer import Layer
-import Activations as ActivationFunctions
-class Dense(Layer):
+from nnlib.Layers import layer
+from nnlib.Activations import sigmoid
+class Dense(layer):
     """
     Camada Densa (ou Totalmente Conectada).
     """
@@ -12,7 +12,7 @@ class Dense(Layer):
         self.output_dim = output_dim
         self.weights = np.random.randn(input_dim, output_dim) * 0.01
         self.bias = np.zeros((1, output_dim))
-        self.activation = ActivationFunctions.activation_function.activation_function()
+        self.activation = activation_function
 
     def forward_pass(self, input_data):
         self.input_data = input_data
@@ -28,5 +28,22 @@ class Dense(Layer):
         # Atualização dos pesos e bias será realizada pelo otimizador
         self.d_weights = d_weights
         self.d_bias = d_bias
-        
+
         return d_input_data
+    
+
+if __name__ == '__main__':
+    # Sample data
+    data = np.array([[1.0, -1.0], [-1.0, 1.0]])
+    activation = sigmoid()
+    
+    # Instantiate Dense layer with LeakyReLU activation
+    dense_layer = Dense(2, 3, activation)
+    
+    # Forward pass
+    output = dense_layer.forward_pass(data)
+    print('Forward Pass Output:', output)
+    
+    # Backward pass
+    gradient = dense_layer.backward_pass(output)#np.array([[1, 1, 1], [1, 1, 1]]))
+    print('Backward Pass Gradient:', gradient)
